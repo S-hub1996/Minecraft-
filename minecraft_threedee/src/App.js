4ccb0235b6d13a32a9b4542b3d0e7e01f9bb4bea
Shubham Verma <94439105+S-hub1996@users.noreply.github.com>
2022-09-26 20:38:17 +0530

    evry

--- minecraft_threedee/src/App.js
+++ minecraft_threedee/src/App.js
@@ -2,25 +2,26 @@ import { Physics } from '@react-three/cannon';
 import { Sky } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import { Ground } from './Components/Ground'
-// import { Player } from './Components/Player'
+import { Player } from './Components/Player'
 import { FPV } from './Components/FPV'
 import { Cubes } from './Components/Cubes'
 import { TextureSelector } from './Components/TextureSelector';
 import { Menu } from './Components/Menu';
 
-
 function App() {
   return (
     <>
-    <div height={'100vh'}>
-
-
-      <Canvas >
-        <Sky sunPosition={[100, 100, 20]} />
+    <div >MineCraft
+      Gameplay:5 types of blocks: Grass, Wood, Log, Glass and Dirt. You switch blocks with numbers 1-5 on your keyboard. You navigate the world with the mouse and WASD. You can click to add blocks and Alt+Click to remove blocks.
+      Made with ReactJS, ThreeJS
+      By. Chhavi Srivastav
+    </div>
+      <Canvas>
+        <Sky sunPosition={[10, 1, 10]} />
         <ambientLight intensity={0.5} />
-        <FPV/>
+        <FPV />
         <Physics>
-          {/* <Player/> */}
+          <Player />
           <Cubes />
           <Ground />
         </Physics>
@@ -28,7 +29,6 @@ function App() {
       <div className='absolute centered cursor'>+</div>
       <TextureSelector />
       <Menu />
-      </div>
     </>
   );
 }
